e1fd0d52b32dd737554ec54fc035febaf79c2c5e
unknown <hannsdewey>
2025-01-07 14:35:26 +0800

    all

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -1,27 +1,32 @@
 @extends('layout')
 @section('content')
+<style>
+.content-wrapper {
+    margin-top: 60px;
+}
+</style>
+<div class="content-wrapper">
+    <div class="card">
+        <div class="card-header">Employee Page</div>
+        <div class="card-body">
 
-<div class="card">
-    <div class="card-header">Employee Page</div>
-    <div class="card-body">
-
-        <form action="{{ url('employee') }}" method="post">
-            {!! csrf_field() !!}
-            <label>Name</label></br>
-            <input type="text" name="name" id="name" class="form-control"></br>
-            <label>Position</label></br>
-            <input type="text" name="position" id="position" class="form-control"></br>
-            <label>Address</label></br>
-            <input type="text" name="address" id="address" class="form-control"></br>
-            <label>Mobile</label></br>
-            <input type="text" name="mobile" id="mobile" class="form-control"></br>
-            <label>Age</label></br>
-            <input type="text" name="age" id="age" class="form-control"></br>
-            <input type="submit" value="Save" class="btn btn-success"></br>
-        </form>
+            <form action="{{ url('employee') }}" method="post">
+                {!! csrf_field() !!}
+                <label>Name</label></br>
+                <input type="text" name="name" id="name" class="form-control"></br>
+                <label>Position</label></br>
+                <input type="text" name="position" id="position" class="form-control"></br>
+                <label>Address</label></br>
+                <input type="text" name="address" id="address" class="form-control"></br>
+                <label>Mobile</label></br>
+                <input type="text" name="mobile" id="mobile" class="form-control"></br>
+                <label>Age</label></br>
+                <input type="text" name="age" id="age" class="form-control"></br>
+                <input type="submit" value="Save" class="btn btn-success"></br>
+            </form>
 
 
+        </div>
     </div>
-</div>
 
-@stop
+    @stop
